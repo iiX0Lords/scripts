251ff205d89a0ab5dd028f3ee62cb2bf105f83c8
Lords <92923519+iiX0Lords@users.noreply.github.com>
2022-10-30 19:40:04 +1100

    Create autofarm.lua

---
+++ autofarm.lua
@@ -0,0 +1,115 @@
+local plr = game.Players.LocalPlayer
+local mouse = plr:GetMouse()
+
+local runservice = game:GetService('RunService')
+local StarterPack = game:GetService('StarterPack')
+local uis = game:GetService('UserInputService')
+local tweenservice = game:GetService('TweenService')
+
+local tycoon = nil
+local cash = plr.leaderstats.Cash
+
+for i,v in pairs(game.workspace.Tycoon.Tycoons:GetChildren()) do
+    if v.Owner.Value == plr then
+       tycoon = v
+    end
+end
+
+
+local cashCollector = tycoon.Essentials.CashCollector
+
+
+
+function rebirth()
+    repeat
+        task.wait()
+        getCash()
+    until cash.Value >= 500000
+    game:GetService("ReplicatedStorage").RebirthEVT:FireServer()
+end
+
+function getButtons()
+    local prices = {}
+    for i,v in pairs(tycoon.UnpurchasedButtons:GetChildren()) do
+        if not v:FindFirstChild("Gamepass") and not v:FindFirstChild("Clothing") then
+            table.insert(prices,v)
+        end
+    end
+
+    if #prices <= 0 then
+        notify("No buttons found (rebirth)",5)
+        rebirth()
+    end
+
+    return prices
+end
+
+function getAllPrices()
+    local prices = {}
+    for i,v in pairs(getButtons()) do
+        table.insert(prices,v.Price.Value)
+    end
+    return prices
+end
+
+function getCash()
+    local df = plr.Character.HumanoidRootPart.CFrame
+    plr.Character.HumanoidRootPart.CFrame = cashCollector.CFrame+Vector3.new(0,5,0)
+    local cashVal = cash.Value
+    local timeout = 0
+    repeat
+        task.wait()
+        timeout = timeout + 0.01
+    until cash.Value ~= cashVal or timeout >= 6
+    plr.Character.HumanoidRootPart.CFrame = df
+end
+
+function buyCheapestButton()
+
+    local cheapest = 1000000
+    local final = nil
+
+    for i,v in pairs(getButtons()) do
+        if v.Price.Value < cheapest then
+            cheapest = v.Price.Value
+            final = v
+        end
+    end
+    
+    if cash.Value > cheapest then
+        local df = plr.Character.HumanoidRootPart.CFrame
+        pcall(function()
+            plr.Character.HumanoidRootPart.CFrame = final.Neon.CFrame
+        end)
+        local timeout = 0
+        repeat
+            task.wait()
+            timeout = timeout + 0.01
+        until not tycoon.UnpurchasedButtons:FindFirstChild(final.Name) or timeout >= 2
+        else
+        getCash()
+    end
+end
+
+function notify(text,duration)
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "Autofarm";
+        Text = text;
+        Duration = duration or 1;
+    })
+end
+
+local enabled = false
+
+uis.InputBegan:Connect(function(input, gameProcessedEvent)
+    if input.KeyCode == Enum.KeyCode.P and not gameProcessedEvent then
+        if enabled then enabled = false else enabled = true end
+        notify(tostring(enabled),2)
+    end
+end)
+
+while task.wait() do
+    if enabled then
+        buyCheapestButton()
+    end
+end
